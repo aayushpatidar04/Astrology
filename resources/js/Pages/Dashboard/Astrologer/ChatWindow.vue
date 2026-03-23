a94add685448ad70026a749a2ce0a893543f3206
Aayush Patidar <aayushpatidar04@gmail.com>
2026-03-23 09:32:45 +0530

    stun + turn

--- resources/js/Pages/Dashboard/Astrologer/ChatWindow.vue
+++ resources/js/Pages/Dashboard/Astrologer/ChatWindow.vue
@@ -103,7 +103,7 @@ const acceptCall = async () => {
 
   pc = new RTCPeerConnection({
     iceServers: [
-      // { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:stun.l.google.com:19302' },
       {
         urls: [
           'turn:openrelay.metered.ca:80',
--- resources/js/Pages/User/ChatWindow.vue
+++ resources/js/Pages/User/ChatWindow.vue
@@ -133,7 +133,7 @@ const startCall = async () => {
 
         pc = new RTCPeerConnection({
             iceServers: [
-                // { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun.l.google.com:19302' },
                 {
                     urls: [
                         'turn:openrelay.metered.ca:80',
